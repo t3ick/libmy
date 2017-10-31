625aa89f50cd030c97911f8c187230ded7987789
t3ick <33251065+t3ick@users.noreply.github.com>
2017-10-31 22:14:59 +0100

    Create my_strdup.c

---
+++ my_strdup.c
@@ -0,0 +1,31 @@
+/*
+** my_strdup.c for my_strdup in /home/t3i/MY_FUNC
+** 
+** Made by NGO tri
+** Login   <ngo_t@etna-alternance.net>
+** 
+** Started on  Tue Oct 24 08:12:46 2017 NGO tri
+** Last update Tue Oct 24 08:38:30 2017 NGO tri
+*/
+
+#include <stdlib.h>
+
+int     my_strlen(char *str);
+
+char *my_strdup(char *str)
+{
+  int i;
+  int size;
+  char *copy;
+  
+  i = 0;
+  size = my_strlen(str);
+  copy = malloc (size);
+  while (str[i])
+    {
+      copy[i] = str[i];
+      i++;
+    }
+  copy[i] = '\0';
+  return (copy);
+}
