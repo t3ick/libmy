e510fa9626398328f80485742c0cc6250c345c7a
t3ick <33251065+t3ick@users.noreply.github.com>
2017-10-31 22:10:28 +0100

    Create my_isneg.c

---
+++ my_isneg.c
@@ -0,0 +1,16 @@
+/*
+** my_isneg.c for my_isneg in /home/t3i/MY_FUNC
+** 
+** Made by NGO tri
+** Login   <ngo_t@etna-alternance.net>
+** 
+** Started on  Mon Oct 23 07:19:08 2017 NGO tri
+** Last update Mon Oct 23 07:22:04 2017 NGO tri
+*/
+int	my_isneg(int n)
+{
+  if (n < 0)
+	 return (1);
+  else
+    return (0);
+}
